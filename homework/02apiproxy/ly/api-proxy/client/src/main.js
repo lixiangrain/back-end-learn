48fe0a2da51b6d8a3b9b010de231082218e0a208
ls-5s <2408224899@qq.com>
2025-08-11 15:00:37 +0800

    feat:初始化和配置Vue应用

---
+++ homework/02apiproxy/ly/api-proxy/client/src/main.js
@@ -0,0 +1,15 @@
+// import './assets/main.css'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import router from './router'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+const pinia = createPinia()
+
+const app = createApp(App)
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
+app.use(router)
+
+app.mount('#app')
